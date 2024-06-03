c331e400f25a4e61ed99713bee10b6aa60ea33de
Geeko24 <kingnorway17@gmail.com>
2024-06-04 01:31:06 +0800

    update

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ import connectDB from './database/config.js'
 
 dotenv.config()
 
-app.use(cors());
+// app.use(cors());
 
 app.use(cors({
     origin: 'http://localhost:5173', // Replace with your frontend domain
