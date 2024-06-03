3864090c84b2059fa462390a4fec9622e5b5c227
Geeko24 <kingnorway17@gmail.com>
2024-06-04 00:57:41 +0800

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: {
-			// eslint-disable-next-line no-undef
-			"@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ command, mode }) => {
+	// Load env file based on `mode` in the current working directory.
+	// Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+	// eslint-disable-next-line no-undef
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		// vite config
+		plugins: [react()],
+		resolve: {
+			alias: {
+				// eslint-disable-next-line no-undef
+				"@": path.resolve(__dirname, "./src"),
+			},
 		},
-	},
-	define: {
-		// eslint-disable-next-line no-undef
-		__APP_ENV__: process.env.VITE_VERCEL_ENV,
-	}
+		define: {
+			// eslint-disable-next-line no-undef
+			__APP_ENV__: JSON.stringify(env.VITE_VERCEL_ENV),
+		},
+	};
 });
